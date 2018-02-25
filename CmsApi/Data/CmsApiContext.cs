f8a6b752436b84a10c3287e9c68d5944049c5e03
Max Vagner <sydneymax@gmail.com>
2018-02-25 17:15:43 +1100

    Upgrade DotNetCore2, models & db context

---
+++ CmsApi/Data/CmsApiContext.cs
@@ -0,0 +1,24 @@
+﻿using CmsApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace CmsApi.Data
+{
+    public class CmsApiContext : DbContext
+    {
+        public CmsApiContext(DbContextOptions<CmsApiContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Page> Pages { get; set; }
+
+        public DbSet<Sidebar> Sidebar { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
---
+++ CmsApi/Models/Page.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsApi.Models
+{
+    public class Page
+    {
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Slug { get; set; }
+
+        public string Content { get; set; }
+
+        public string HasSidebar { get; set; }
+
+    }
+}
